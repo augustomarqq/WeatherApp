bc06edd34fd05bc950f1055c7bcb2302527d8f5a
augustomarqq <augustomarqq@gmail.com>
2025-06-18 00:58:44 -0300

    Prática 02 – fix: remove unused imports

--- app/src/main/java/com/weatherapp/LoginActivity.kt
+++ app/src/main/java/com/weatherapp/LoginActivity.kt
@@ -10,7 +10,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
