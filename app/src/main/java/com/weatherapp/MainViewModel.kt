a4991bae601ef3daba49c21d619335175de848c9
augustomarqq <augustomarqq@gmail.com>
2025-06-25 22:16:17 -0300

    Prática 04 – Parte 2: Refatoração e Permissões

--- app/src/main/java/com/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/weatherapp/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.weatherapp
 
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
 import com.weatherapp.model.City
 
 class MainViewModel : ViewModel() {
@@ -13,8 +14,8 @@ class MainViewModel : ViewModel() {
         _cities.remove(city)
     }
 
-    fun add(name: String) {
-        _cities.add(City(name = name))
+    fun add(name: String, location: LatLng? = null) {
+        _cities.add(City(name = name, location = location))
     }
 }
 
