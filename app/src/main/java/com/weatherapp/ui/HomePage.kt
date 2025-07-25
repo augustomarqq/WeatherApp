c70f4964184e6ad3ca0051eb25c3a08f67ecdab9
augustomarqq <augustomarqq@gmail.com>
2025-07-25 18:48:06 -0300

    Prática 09 – fix: removendo imports não utilizados

--- app/src/main/java/com/weatherapp/ui/HomePage.kt
+++ app/src/main/java/com/weatherapp/ui/HomePage.kt
@@ -9,9 +9,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
