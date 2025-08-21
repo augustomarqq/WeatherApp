689c1fb577c9c989528aec27283361d677edb493
augustomarqq <augustomarqq@gmail.com>
2025-08-21 02:07:29 -0300

    Prática 12 – Parte 3: Atualizando o Repository e o ViewModel

--- app/src/main/java/com/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/weatherapp/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.LatLng
 import com.weatherapp.api.WeatherService
 import com.weatherapp.api.toForecast
@@ -13,126 +14,108 @@ import com.weatherapp.model.User
 import com.weatherapp.monitor.ForecastMonitor
 import com.weatherapp.repo.Repository
 import com.weatherapp.ui.nav.Route
+import kotlinx.coroutines.*
 
 class MainViewModel (private val repository: Repository,
                      private val service: WeatherService,
-                     private val monitor: ForecastMonitor): ViewModel(), Repository.Listener {
-        private val _cities = mutableStateMapOf<String, City>()
-        val cities : List<City>
-            get() = _cities.values.toList()
+                     private val monitor: ForecastMonitor): ViewModel() {
+    private val _cities = mutableStateMapOf<String, City>()
+    val cities: List<City>
+        get() = _cities.values.toList()
 
-        private var _city = mutableStateOf<City?>(null)
-        var city: City?
-            get() = _city.value
-            set(tmp) { _city.value = tmp?.copy() }
-
-        private val _user = mutableStateOf<User?> (null)
-        val user : User? get() = _user.value
-
-        private var _page = mutableStateOf<Route>(Route.Home)
-        var page: Route
-            get() = _page.value
-            set(tmp) { _page.value = tmp }
-
-        init {
-            repository.setListener(this)
+    private var _city = mutableStateOf<City?>(null)
+    var city: City?
+        get() = _city.value
+        set(tmp) {
+            _city.value = tmp?.copy()
         }
 
-        fun remove(city: City) {
-            repository.remove(city)
+    private val _user = mutableStateOf<User?>(null)
+    val user: User? get() = _user.value
+
+    private var _page = mutableStateOf<Route>(Route.Home)
+    var page: Route
+        get() = _page.value
+        set(tmp) {
+            _page.value = tmp
         }
 
-        fun update(city: City) {
-            repository.update(city)
-        }
-
-        fun add(name: String) {
-            service.getLocation(name) { lat, lng ->
-                if (lat != null && lng != null) {
-                    repository.add(City(name = name, location = LatLng(lat, lng)))
-                }
+    init {
+        viewModelScope.launch(Dispatchers.Main) {
+            repository.user.collect { user ->
+                _user.value = user.copy()
             }
         }
 
-        fun add(location: LatLng) {
-            service.getName(location.latitude, location.longitude) { name ->
-                if (name != null) {
-                    repository.add(City(name = name, location = location))
-                }
+        viewModelScope.launch(Dispatchers.Main)
+        {
+            repository.cities.collect { list ->
+                val names = list.map { it.name }
+                val newCities = list.filter { it.name !in _cities.keys }
+                val oldCities = list.filter { it.name in _cities.keys }
+                _cities.keys.removeIf { it !in names } // remove cidades deletadas
+                newCities.forEach { _cities[it.name] = it } // adiciona cidades novas
+                oldCities.forEach { refresh(it) }
             }
         }
-
-        fun loadWeather(name: String) {
-            service.getWeather(name) { apiWeather ->
-                val newCity = _cities[name]!!.copy(weather = apiWeather?.toWeather())
-                _cities.remove(name)
-                _cities[name] = newCity
-                }
+    }
+            fun remove(city: City) {
+                repository.remove(city)
             }
 
-        fun loadForecast(name: String) {
-            service.getForecast(name) { apiForecast ->
-                val newCity = _cities[name]!!.copy( forecast = apiForecast?.toForecast())
-                _cities.remove(name)
-                _cities[name] = newCity
-                city = if (city?.name == name) newCity else city
+            fun update(city: City) {
+                repository.update(city)
             }
-        }
 
-        fun loadBitmap(name: String) {
-            val city = _cities[name]
-            service.getBitmap(city?.weather!!.imgUrl) {bitmap ->
-                val newCity = city.copy(
-                    weather = city.weather?.copy(
-                        bitmap = bitmap
+            fun add(name: String) = viewModelScope.launch(Dispatchers.IO) {
+                val location = service.getLocation(name)?:return@launch
+                repository.add(City( name = name, location = location))
+            }
+
+            fun add(location: LatLng) = viewModelScope.launch(Dispatchers.IO) {
+                val name = service.getName(location.latitude, location.longitude)?:return@launch
+                repository.add(City( name = name, location = location))
+            }
+
+            fun loadWeather(name: String) = viewModelScope.launch(Dispatchers.IO) {
+                val weather = service.getWeather(name)?.toWeather()
+                _cities[name]?.let { refresh(it.copy(weather = weather)) }
+            }
+
+            fun loadForecast(name : String) = viewModelScope.launch(Dispatchers.IO) {
+                val forecast = service.getForecast(name)?.toForecast()
+                _cities[name]?.let { refresh(it.copy(forecast = forecast)) }
+            }
+
+            fun loadBitmap(imgUrl: String) = viewModelScope.launch(Dispatchers.IO) {
+                val bitmap = service.getBitmap(city!!.weather!!.imgUrl)
+                _cities[imgUrl]?.let { refresh(it.copy(weather = it.weather!!.copy(bitmap = bitmap))) }
+            }
+
+            suspend fun refresh(city: City) = withContext(Dispatchers.Main) {
+                val oldCity = _cities[city.name]
+                _cities.remove(city.name)
+                _cities[city.name] = city.copy(
+                    weather = city.weather?:oldCity?.weather, // se novo = null, reusa antigo
+                    forecast = city.forecast?:oldCity?.forecast, // se novo = null, reusa antigo
                     )
-                )
-                _cities.remove(name)
-                _cities[name] = newCity
+                if (_city.value?.name == city.name) {
+                    _city.value = _cities[city.name]
+                }
+                monitor.updateCity(_cities[city.name]!!)
             }
         }
 
-        override fun onUserLoaded(user: User) {
-            _user.value = user
-        }
-
-        override fun onUserSignOut() {
-            monitor.cancelAll()
-        }
-
-        override fun onCityAdded(city: City) {
-            _cities[city.name] = city
-            monitor.updateCity(city)
-        }
-
-        override fun onCityUpdated(city: City) {
-            val oldCity = _cities[city.name]
-            _cities.remove(city.name)
-            _cities[city.name] = city.copy(
-                weather = oldCity?.weather,
-                forecast = oldCity?.forecast
-            )
-            if (_city.value?.name == city.name) {
-                _city.value = _cities[city.name]
+            class MainViewModelFactory(private val repository : Repository,
+                                       private val service: WeatherService,
+                                       private val monitor: ForecastMonitor) :
+                ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                        return MainViewModel(repository, service, monitor) as T
+                    }
+                    throw IllegalArgumentException("Unknown ViewModel class")
+                }
             }
-            monitor.updateCity(city)
-        }
 
-        override fun onCityRemoved(city: City) {
-            _cities.remove(city.name)
-            if (_city.value?.name == city.name) { _city.value = null }
-            monitor.cancelCity(city)
-        }
-    }
 
-class MainViewModelFactory(private val repository : Repository,
-                           private val service: WeatherService,
-                           private val monitor: ForecastMonitor) :
-    ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-                return MainViewModel(repository, service, monitor) as T
-            }
-            throw IllegalArgumentException("Unknown ViewModel class")
-        }
-    }
